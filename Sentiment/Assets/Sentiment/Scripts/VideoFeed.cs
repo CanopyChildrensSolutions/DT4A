e50a8bee385581c678ec060f6b60caa6dc0e8574
Josh Wittner <jowitt@microsoft.com>
2018-01-06 14:29:12 -0800

    View video feed and save to png on device with airtap

---
+++ Sentiment/Assets/Sentiment/Scripts/VideoFeed.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sentiment
+{
+    public class VideoFeed : MonoBehaviour
+    {
+        WebCamTexture webCam;
+
+        public bool AutoStart = true;
+
+        public Texture LiveTexture {  get { return webCam; } }
+
+        public byte[] EncodeCurrentFrameAsPNG()
+        {
+            var texture = new Texture2D(webCam.width, webCam.height, TextureFormat.ARGB32, false);
+            texture.SetPixels32(webCam.GetPixels32());
+            return texture.EncodeToPNG();
+        }
+
+        private void Awake()
+        {
+            webCam = new WebCamTexture();
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            if( AutoStart)
+            {
+                Play();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (webCam != null)
+            {
+                if (webCam.isPlaying)
+                {
+                    webCam.Stop();
+                }
+
+                webCam = null;
+            }
+        }
+
+        public void Play()
+        {
+            webCam.Play();
+        }
+
+        public void Stop()
+        {
+            webCam.Stop();
+        }
+    }
+}
---
+++ Sentiment/Assets/Sentiment/Scripts/VideoViewTexture.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sentiment
+{
+    [RequireComponent(typeof(Renderer))]
+    public class VideoViewTexture : MonoBehaviour
+    {
+        void Start()
+        {
+            var videoFeed = GetComponentInParent<VideoFeed>();
+            var renderer = GetComponent<Renderer>();
+
+            var mpb = new MaterialPropertyBlock();
+            renderer.GetPropertyBlock(mpb);
+
+            mpb.SetTexture("_MainTex", videoFeed.LiveTexture);
+
+            renderer.SetPropertyBlock(mpb);
+        }
+    }
+}
